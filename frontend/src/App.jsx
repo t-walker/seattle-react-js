007b7dd6b349f6b24fff2efa106942c9c451a679
Tyler Walker <twalkerweb@gmail.com>
2019-02-10 16:50:23 -0800

    feat: added routing (#3)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import {Container} from 'reactstrap';
+import { Switch, Route } from 'react-router-dom'
+
 import Navigation from './components/shared/Navigation';
 import Header from './components/shared/Header';
+
+import config from './config';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <Header />
-          <Navigation />
+        <Header />
+        <Navigation />
+        <Switch>
+          {config.pages.map((page) => {
+            return <Route exact path={page.path} component={page.component} />
+          })}
+        </Switch>
       </div>
     )
   }
